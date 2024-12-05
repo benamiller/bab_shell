5b0351eb4c4ea3450cdd640965429d1a7a01a1d1
benamiller <mrbenamiller@gmail.com>
2024-12-05 19:14:39 +0000

    Getting path and list of directories to search through for typed bin

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -10,6 +10,17 @@ import (
 
 var _ = fmt.Fprint
 
+func find_directory(command string) (string, bool) {
+	path := os.Getenv("PATH")
+	directories := strings.Split(path, ":")
+
+	for index, value := range directories {
+		fmt.Println(index, value)
+	}
+
+	return "test/", true
+}
+
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 
@@ -47,7 +58,10 @@ func main() {
 			if exists {
 				fmt.Println(type_queried)
 			} else {
-				fmt.Fprintf(os.Stdout, "%s: not found\n", commands[1])
+				pathToCommand, exists := find_directory(commands[1])
+				if exists {
+					fmt.Fprintf(os.Stdout, "%s is %s\n", commands[1], pathToCommand)
+				}
 			}
 		default:
 			fmt.Fprintf(os.Stdout, "%s: command not found\n", input)
